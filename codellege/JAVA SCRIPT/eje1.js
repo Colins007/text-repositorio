54e6a6ef7a358b732f72d764357e18814df60eb0
yasser collins <ycollin100837@ppsc.ued.mx>
2023-03-24 15:35:58 -0600

    commit_start

---
+++ codellege/JAVA SCRIPT/eje1.js
@@ -0,0 +1,6 @@
+let ahorro = 0
+for(let i=1;i<=365;i++){
+    ahorro=ahorro+(Math.pow(3,))
+    console.log('ahorro en el dia ${i} es de: ${Math.pow(3,i)} ')
+}
+console.log('el ahorro anual es de: ${}')
